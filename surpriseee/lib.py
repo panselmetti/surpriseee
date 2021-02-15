f8ce198bca8ed374f5b65ba5a02a4cb754eed7e5
pierre anselmetti <pierre.anselmetti@gmail.com>
2021-02-15 15:32:10 +0100

    commited

--- surpriseee/lib.py
+++ surpriseee/lib.py
@@ -1,2 +1,8 @@
+import webbrowser
+
 def try_me():
-    return 'https://giphy.com/gifs/somegoodnews-we-did-it-wedidit-its-done-LpQrsRA3zOuJNk7KYt'
+    webbrowser.open_new_tab('https://giphy.com/gifs/somegoodnews-we-did-it-wedidit-its-done-LpQrsRA3zOuJNk7KYt')
+
+
+if __name__ == "__main__":
+    webbrowser.open_new_tab('https://giphy.com/gifs/somegoodnews-we-did-it-wedidit-its-done-LpQrsRA3zOuJNk7KYt')
